9f62bc95313c1a27fe67287ae48aadee4d0db1b0
cheeky-lord <lucasilva108@gmail.com>
2019-07-23 23:43:12 -0300

    setting connectionString

--- ClassRoomSpace.Infra/Context/Settings.cs
+++ ClassRoomSpace.Infra/Context/Settings.cs
@@ -2,6 +2,7 @@ namespace ClassRoomSpace.Infra.Context
 {
     public static class Settings
     {
-        public static string ConnectionString = @"Server=LUCAS-CDS;Database=classroomspace;Integrated Security=true;User Id=sa;Password=123;";
+        // public static string ConnectionString = @"Server=LUCAS-CDS;Database=classroomspace;Integrated Security=true;User Id=sa;Password=123;";
+        public static string ConnectionString = @"Server=DESKTOP-H7HI9UU;Database=classroomspace;Integrated Security=true;User Id=sa;Password=123;";
     }
 }
